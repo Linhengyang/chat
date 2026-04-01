617f26d2a3f99dc05e6f8022a4e655432d93b6f2
lhy-Macbook01 <linhengyang1@outlook.com>
2026-04-01 11:01:39 +0800

    update ask_qwen

--- qwen.py
+++ qwen.py
@@ -2,32 +2,39 @@ import os
 from openai import OpenAI
 import json
 
+# 模型列表: https://help.aliyun.com/model-studio/getting-started/models
+
 client = OpenAI(
     api_key = os.getenv("DASHSCOPE_API_KEY"),
     base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-conversation_fp = 'test.json'
-
-with open(conversation_fp) as f:
-    conversation_history = json.load(f)
 
 
-user_input = \
-"""OpenAI的SDK里, llm call有四个role分别是system、user、assistant、tool，共同组成了上下文。先不管tool，想问你哪个是大家常说的prompt？system和assistant区别在哪？"""
+chatlogs = 'test.json'
+
+question = \
+"""
+OpenAI的SDK里, llm call有四个role分别是system、user、assistant、tool，共同组成了上下文。先不管tool，想问你哪个是大家常说的prompt？system和assistant区别在哪？
+"""
+question = question.strip()
 
 
-def chat():
 
-    conversation_history.append({
+def ask_qwen(chatlogs_json, user_query):
+
+    with open(chatlogs_json) as f:
+        conversation_context = json.load(f)
+
+    conversation_context.append({
         "role": "user", 
-        "content": user_input
+        "content": user_query
     })
 
     try:
         response = client.chat.completions.create(
-            model="qwen3.5-plus",               # 模型列表: https://help.aliyun.com/model-studio/getting-started/models
-            messages=conversation_history
+            model = "qwen3.5-plus",
+            messages = conversation_context
         )
 
     except Exception as e:
@@ -39,10 +46,10 @@ def chat():
         "content": response.choices[0].message.content
     }
 
-    conversation_history.append(llm_reply)
+    conversation_context.append(llm_reply)
 
-    with open(conversation_fp, 'w') as f:
-        json.dump(conversation_history, f, indent=4, ensure_ascii=False)
+    with open(chatlogs_json, 'w') as f:
+        json.dump(conversation_context, f, indent=4, ensure_ascii=False)
     
     return llm_reply['content']
 
@@ -51,4 +58,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    print(chat(), file='render.md')
+    print(ask_qwen(chatlogs, question), file='render.md')
